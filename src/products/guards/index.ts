94aab9ebf314a0b7a78a29b20dda881c47d8241d
Stepan <stepn1k@yandex.by>
2020-10-05 09:33:30 +0300

    updated load to guards

--- src/products/guards/index.ts
+++ src/products/guards/index.ts
@@ -1,5 +1,9 @@
 import {PizzasGuard} from "./pizza.guard";
+import {PizzaExistsGuards} from "./pizza-exists.guard";
+import {ToppingsGuard} from "./toppings.guard";
 
-export const guards: any[] = [PizzasGuard];
+export const guards: any[] = [PizzasGuard, PizzaExistsGuards, ToppingsGuard];
 
 export * from "./pizza.guard"
+export * from './pizza-exists.guard'
+export * from "./toppings.guard"
